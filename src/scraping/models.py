9b0a5067c8fc617eaa803050e37427eb39169c35
VladKitTort <vovatortik@mail.ru>
2020-09-20 21:04:08 +0500

    Migrations of the "ComputerLanguage" app.

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -2,12 +2,26 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Наименование населенного пункта")
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name="Наименование населенного пункта",
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = "Наименование населенного пункта"
-        verbose_name_plural = "Наименование населенных пунктов"
+        verbose_name_plural = "Наименование  населенных пунктов"
+
+    def __str__(self):
+        return self.name
+
+
+class ComputerLanguage(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Язык програмирования",
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = "Язык програмирования"
+        verbose_name_plural = "Языки програмирования"
 
     def __str__(self):
         return self.name
