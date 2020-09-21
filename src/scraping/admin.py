196b574a0f39617f96e26c5ad119178c5b88562d
VladKitTort <vovatortik@mail.ru>
2020-09-21 19:51:10 +0500

    Creating the "Vacancy" app.

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import City, ComputerLanguage
+from .models import City, ComputerLanguage, Vacancy
 
 admin.site.register(City)
 admin.site.register(ComputerLanguage)
+admin.site.register(Vacancy)
